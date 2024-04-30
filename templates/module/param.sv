c51669b5d85494773e8b843632bc6b567f78e2e7
JJ-hw <110426712+JJ-hw@users.noreply.github.com>
2024-04-30 21:21:14 +0200

    [CLEANUP] Fix comma position

--- templates/module/param.sv
+++ templates/module/param.sv
@@ -1,5 +1,6 @@
 {# Template for generating RTL parameter #}
 {# Warn: Keep indentation in phase with module template (cf. param_snippets) #}
 {% for d in default_name %}
-    ,parameter int {{ d }}
+    parameter int {{ d }} = 0
+    {%- if loop.last %}{% break %}{% endif -%},
 {% endfor %}
